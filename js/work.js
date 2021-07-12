b0c117dc79ad198a60a82c9ecc4c9d81aeee4810
Kindra Lingenfelter <github@kindra.dev>
2021-07-12 16:30:58 -0600

    completed basic jquery for mobile menu

---
+++ js/work.js
@@ -0,0 +1,16 @@
+$(".navIcon").on("click", function(){
+    // alert("click");
+    if($(this).hasClass("closed"))
+    {
+        // alert("closed");
+        $(this).attr("src", "images/mobile_button_pressed.svg").removeClass("closed");
+        $("nav").css("visibility", "visible");
+    }
+    else
+    {
+        // alert("open");
+        $(this).attr("src", "images/mobile_button_normal.svg").addClass("closed");
+        $("nav").css("visibility", "hidden");
+    }
+
+});
