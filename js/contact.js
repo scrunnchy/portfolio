50cf02d27d4fae45ff96a3b6b6e6bbbde9ffcc2c
Kindra Lingenfelter <github@kindra.dev>
2021-07-13 14:15:01 -0600

    made more bugs while trying to fix bugs >:(

--- js/contact.js
+++ js/contact.js
@@ -18,3 +18,36 @@ $(".navIcon").on("click", function(){
 if($(window).width() > 1000){
     $("nav").css("visibility", "visible");
 }
+
+//Get the button:
+mybutton = document.getElementById("scrollIcon");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
+
+
+$(".button").hover(function(){
+  // alert("hit");
+  if($(this).hasClass("normal"))
+  {
+    $(".button").css("backgroundImage", "url('../images/button_hover.svg')").removeClass("normal");
+  }
+  else
+  {
+    $(".button").css("backgroundImage", "url('../images/button_normal.svg')").addClass("normal");
+  }
+});
