8e85a763e6c8900f169c2a70411538ab546a1704
Kindra Lingenfelter <github@kindra.dev>
2021-07-20 19:38:46 -0600

    updated google analytics

--- js/sharedScripts.js
+++ js/sharedScripts.js
@@ -27,17 +27,14 @@ window.onresize = function(){
   }
 }
 
-//Get the button:
-mybutton = document.getElementById("scrollIcon");
-
 // When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    $("#scrollIcon").css("display", "block");
   } else {
-    mybutton.style.display = "none";
+    $("#scrollIcon").css("display", "none");
   }
 }
 
